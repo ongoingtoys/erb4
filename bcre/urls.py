96f5708ca1f4fcd148569703ea10bcdc21a8ffbf
ongoingtoys <ongoingtoys@gmail.com>
2024-12-16 15:40:56 +0800

    Step 2 build pages app

--- bcre/urls.py
+++ bcre/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include     #url = route to dif app or site
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include('pages.urls')),     #page= app
+    path('admin/', admin.site.urls),     #admin.site= virtual environment  
 ]
